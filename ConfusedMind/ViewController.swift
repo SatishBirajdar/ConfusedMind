7f9a22d5b8e725f0a5bd645f5c6701579bb3721c
Satish Birajdar <satishbirajdar1@gmail.com>
2017-08-22 22:47:04 -0400

    Initial Commit

---
+++ ConfusedMind/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ConfusedMind
+//
+//  Created by Satish Birajdar on 2017-08-22.
+//  Copyright © 2017 SBSoftwares. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
